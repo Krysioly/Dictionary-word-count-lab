acd48d8deab199aad236c4d9b2435997781e1e27
Hackbright Student <no-reply@hackbrightacademy.com>
2018-09-10 12:24:15 -0700

    wordcount.py working, removes punctuation

--- wordcount.py
+++ wordcount.py
@@ -1,5 +1,7 @@
 # put your code here.
 
+import string
+
 def find_word_count(input_file):
     file = open(input_file)
     words_dict = {}
@@ -8,6 +10,9 @@ def find_word_count(input_file):
         line_words = line.split()
         # line_words = line.split(" ")
         for word in line_words:
+            for c in word:
+                if c in string.punctuation:
+                    word= word.strip(c)
             words_dict[word] = words_dict.get(word, 0) + 1
 
     for word, count in words_dict.items():
