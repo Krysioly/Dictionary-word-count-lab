9c9e44591bc7d3b8b8163c36e1f3fae72661e990
Hackbright Student <no-reply@hackbrightacademy.com>
2018-09-10 13:53:14 -0700

    wordcount.py working with collections, stripping punctuation not working

--- wordcount.py
+++ wordcount.py
@@ -2,26 +2,43 @@
 
 import string
 import sys
+import collections
 
 def find_word_count(input_file):
-    """
+    """Inputs file and counts number of times each word appears
+
+    Removes punctuation and capitalization
 
     """
-    file = open(input_file)
-    words_dict = {}
+    # file = open(input_file)
+    # words_dict = {}
 
-    for line in file:
-        line_words = line.split()
-        # line_words = line.split(" ")
-        for word in line_words:
-            word = word.lower()
-            for c in word:
-                if c in string.punctuation:
-                    word= word.strip(c)
-            words_dict[word] = words_dict.get(word, 0) + 1
+    # for line in file:
+    #     line_words = line.split()
+    #     # line_words = line.split(" ")
+    #     for word in line_words:
+    #         word = word.lower()
+    #         for c in word:
+    #             if c in string.punctuation:
+    #                 word= word.strip(c)
+    #         words_dict[word] = words_dict.get(word, 0) + 1
 
-    for word, count in words_dict.items():
+    # for word, count in words_dict.items():
+    #     print("{} {}".format(word, count))
+
+    file_string = open(input_file).read()
+    text = file_string.lower()
+
+    # This is not working!!!!
+    for c in text:
+        if c in string.punctuation:
+            text=text.strip(c)
+
+    words = text.split()
+
+    for word, count in collections.Counter(words).items():
         print("{} {}".format(word, count))
+
 
 find_word_count(sys.argv[1])
 
