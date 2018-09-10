2e9cbacb5bcc3e165c1c30295325c8eb1aa2d7a9
Hackbright Student <no-reply@hackbrightacademy.com>
2018-09-10 12:10:51 -0700

    wordcount.py working.  No punctuation or capitalization considered

---
+++ wordcount.py
@@ -0,0 +1,17 @@
+# put your code here.
+
+def find_word_count(input_file):
+    file = open(input_file)
+    words_dict = {}
+
+    for line in file:
+        line_words = line.rstrip().split()
+        # line_words = line.split(" ")
+        for word in line_words:
+            words_dict[word] = words_dict.get(word, 0) + 1
+
+    for word, count in words_dict.items():
+        print("{} {}".format(word, count))
+
+find_word_count("test.txt")
+
